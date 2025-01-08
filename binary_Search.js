6f068d80faf64bec79d3d25bdb9b6074de73c45a
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-01-08 13:08:55 +0500

    binary_search done

---
+++ binary_Search.js
@@ -0,0 +1,17 @@
+const my_list = [1, 3, 5, 7, 9];
+function binary_search (list, item){
+    let low = 0;
+    let high = list.length-1;
+    while(low <= high){
+        let mid = Math.floor((low+high)/2)
+        let guess = list[mid]
+        if(guess===item){
+            return mid
+        }else if(guess>item){
+            high = mid - 1
+        }else {
+            low = mid +1
+        }
+    } return `${item} does not exist`
+}
+console.log(binary_search( my_list,5))
