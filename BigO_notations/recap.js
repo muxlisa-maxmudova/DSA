da2d3c3f5fb9a495acaa6e58494864359b1d2b02
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-09 14:18:31 +0500

    vending machine done!

---
+++ BigO_notations/recap.js
@@ -0,0 +1,13 @@
+// const arr = ['hello', 'world'];
+// arr.map((item, index) => {
+//     console.log(item, index);
+// })
+// let a = 25
+// a = a.toString()
+// console.log(typeof a)
+// let a = prompt('What is your name?')
+// console.log(a)
+a = 27
+console.log(Math.pow(a, 2))
+console.log(Math.cbrt(a))
+
