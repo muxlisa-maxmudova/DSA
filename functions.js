658ce155fcc3462fb282bceab73fdac48785f3ca
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-23 20:03:13 +0500

    compared loops in js and in py

---
+++ functions.js
@@ -0,0 +1,4 @@
+function greeting(temp){
+    console.log(temp.toUpperCase())
+}
+greeting('Mukhlisa')
---
+++ loops.js
@@ -0,0 +1,4 @@
+students = ['Henry', 'Ron', 'Matilda']
+for (let student in students){
+    console.log(students[student]);
+} //we have to initialize in js since student is showcasing the index of each element
