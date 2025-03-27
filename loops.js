d4775d70e09295f0e9b4b016a3f13bc14fa8327c
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-03-27 22:07:41 +0500

    compared loops in js and in py again

--- loops.js
+++ loops.js
@@ -1,4 +1,23 @@
-students = ['Henry', 'Ron', 'Matilda']
-for (let student in students){
-    console.log(students[student]);
-} //we have to initialize in js since student is showcasing the index of each element
+// students = ['Henry', 'Ron', 'Matilda']
+// for (let student in students){
+//     console.log(students[student]);
+// } //we have to initialize in js since student is showcasing the index of each element
+// const students = {
+//     Hermione: 'Gryffindor',
+//     Harry: 'Gryffindor'
+// }
+//  const  history = ['ddddnwed', 0, 'ddneiew']
+// function main(){
+//     while(history.length<3){
+//         let action = prompt('Action?')
+//         history.push(action)
+//     }
+// }
+// main()
+// console.log(history)
+const main = 'Hello'
+const s = main.split("")
+console.log(s)
+
+
+// console.log(main.slice(0,3))
