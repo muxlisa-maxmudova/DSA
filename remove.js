4a4824043448105bece6fcef6642aa617db426f6
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-05-21 22:31:04 +0500

    updated

--- remove.js
+++ remove.js
@@ -1,13 +1,21 @@
-function removeDuplicates(head){
-    let nums = 0;
-    let newList = [];
-    for (i = 0; i < head.length; i++) {
-        elem = head[i]
-        if(elem !== head[i+1]){
-            nums = elem
-            newList.push(nums)
-        }
-    }
-    console.log(newList)
-}
-removeDuplicates([1,1,2]) // check for sorted list
+// function removeDuplicates(head){
+//     let nums = 0;
+//     let newList = [];
+//     for (i = 0; i < head.length; i++) {
+//         elem = head[i]
+//         if(elem !== head[i+1]){
+//             nums = elem
+//             newList.push(nums)
+//         }
+//     }
+//     console.log(newList)
+// }
+// removeDuplicates([1,1,2]) // check for sorted list
+// total = 0
+// for (let i=0; i <= 1000; i++) {
+//     if (i.toString().includes('3') && i.toString().includes('5')) {
+//         console.log(i);
+//         total++
+//     }
+// }
+// console.log(`total is ${total}`);
